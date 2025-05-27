29fae367026fa3267f21703d0928b8e97cf6b155
rohail2012 <72384686+rohail2012@users.noreply.github.com>
2025-05-27 13:22:37 +0500

    new change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://modern-sw.github.io',
+  url: 'https://rohail2025.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/lab1/',
